8d7614696da55e015648081b3c17c53c15d7b78b
Musharaf Shaik <musharafshaik630@gmail.com>
2026-08-05 12:40:56 +0530

    express.static for public folder

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { createServer } from "node:http";
+import path from "path";
 import express from "express";
 
 async function main() {
     const PORT = process.env.PORT ?? 8000;
 
     const app = express();
+    app.use(express.static(path.resolve("./public")));
+    //express.static first check for routes , if no routes found then go to public folder by default
+
     const httpServer = createServer(app);
 
     app.get("/health", (req, res) => {
