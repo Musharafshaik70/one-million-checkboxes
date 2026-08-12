bc8f6f22752f63943de757d49522773e25163f59
Musharaf Shaik <musharafshaik630@gmail.com>
2026-08-13 02:21:37 +0530

    feat: support one million checkboxes without horizontal scaling

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import { createServer } from "node:http";
 import path from "path";
+
 import express from "express";
+import { Server } from "socket.io";
+
+const CHECKBOX_COUNT = 100;
+const state = {
+    checkboxes: new Array(CHECKBOX_COUNT).fill(false),
+};
 
 async function main() {
     const PORT = process.env.PORT ?? 8000;
@@ -11,8 +18,28 @@ async function main() {
 
     const httpServer = createServer(app);
 
+    //socket io handlers
+    const io = new Server();
+    io.attach(httpServer);
+
+    io.on("connection", (socket) => {
+        console.log("Socket Connected", { id: socket.id });
+
+        socket.on("client:checkbox:changed", (data) => {
+            console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
+            const { index, checked } = data;
+            state.checkboxes[index] = checked;
+            io.emit("server:checkbox:changed", data);
+        });
+    });
+
+    //express handlers
     app.get("/health", (req, res) => {
         return res.json({ healthy: true });
+    });
+
+    app.get("/checkboxes", (req, res) => {
+        return res.status(200).send({ checkboxes: state.checkboxes });
     });
 
     httpServer.listen(PORT, () => {
